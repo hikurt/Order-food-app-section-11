30b6affac36745a619a775bcb7b4ecd527e9b6c9
hikurt <32220244+hikurt@users.noreply.github.com>
2023-02-02 13:46:05 +0100

    Can cancel cart via button and clicking on dropback

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Modal from '../UI/Modal';
+
+import classes from './Cart.module.css';
+
+
+const Cart = (props) => {
+  const cartItems = <ul className={classes['cart-item']}>
+    {
+      [
+        {
+          id: 'c1',
+          name: 'Sushi',
+          amount: '2',
+          price: '50.99'
+        },
+        {
+          id: 'c2',
+          name: 'Burger',
+          amount: '3',
+          price: '30.99'
+        },
+      ].map(item => <li>{item.name}</li>)
+    }
+  </ul>;
+
+  return (
+    <Modal onClose={props.onClose}>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>35.45</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </Modal>
+  );
+};
+
+export default Cart;
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,11 +2,11 @@ import React, {Fragment} from 'react';
 import classes from './Header.module.css';
 import HeaderCartButton from './HeaderCartButton';
 
-const Header = () => {
+const Header = (props) => {
   return <Fragment>
     <header className={classes.header}>
       <h1>ReactMeals</h1>
-      <HeaderCartButton />
+      <HeaderCartButton onClick={props.onShowCart}/>
     </header>
     <div className={classes['main-image']}>
       <img src='https://img.freepik.com/premium-photo/group-friends-eating-together_53876-9934.jpg?w=826' alt='A table full of delicious food!'/>
